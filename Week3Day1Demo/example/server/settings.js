8a5fc1aecb9a52880928872f7917edc6b23a48f6
Pavanitha <p.jampala.1193@westcliff.edu>
2026-03-16 21:17:14 -0700

    week 3 Day 1 demo

---
+++ Week3Day1Demo/example/server/settings.js
@@ -0,0 +1,9 @@
+module.exports = {
+  port: 5000,
+  oauth: {
+    tokenUrl: 'https://github.com/login/oauth/access_token',
+    clientId: '0v23liByiI6JdK0Ppqyo',
+    clientSecret: 'PASTE_YOUR_CLIENT_SECRET_HERE',
+    redirectUri: 'http://localhost:1234/',
+  },
+};
